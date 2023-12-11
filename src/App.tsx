7c09a7ab623f58fc462e6f91486d7ee2879d68a0
Caleb Rivera <calebrossr@gmail.com>
2023-12-11 03:43:22 -0500

    create and render AuthButton component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,35 @@
+import { useAuth0 } from "@auth0/auth0-react";
+
+function AuthButton() {
+  const { loginWithRedirect, isAuthenticated, isLoading, logout } = useAuth0();
+
+  if (isLoading) return <div>Loading...</div>;  
+
+  if (isAuthenticated) {
+    return (
+      <button
+        onClick={() =>
+          logout({
+            logoutParams: {
+              returnTo: window.location.origin,
+            },
+          })
+        }
+      >
+        Log out
+      </button>
+    );
+  }
+
+  return <button onClick={() => loginWithRedirect()}>Log in</button>;
+}
+
 function App() {
-  return <>Hello, World!</>;
+  return (
+    <>
+      <AuthButton />
+    </>
+  );
 }
 
 export default App;
